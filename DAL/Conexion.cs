6ae783ddb669221a1e3f166e4bdfe5c69821d4f8
CNNICO7 <nicolassofiahillar@gmail.com>
2026-04-15 13:31:43 -0300

    Implementacion inicial de SQL

---
+++ DAL/Conexion.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DAL
+{
+    public class Conexion
+    {
+        private readonly string _connectionString = "Server=.;DataBase=BDProyecto;Integrated Security=True";
+    
+        public DataTable Leer(string query, Dictionary<string, object> parametros = null, bool StoredProcedure = false)
+        {
+            SqlConnection connection = new SqlConnection(_connectionString);
+            connection.Open();
+            SqlCommand cm = new SqlCommand();
+            using (cm = new SqlCommand(query, connection))
+            {
+                if (parametros != null)
+                {
+                    foreach (var p in parametros)
+                    {
+                        cm.Parameters.AddWithValue(p.Key, p.Value ?? DBNull.Value);
+                    }
+                }
+                using (SqlDataAdapter da = new SqlDataAdapter(cm))
+                {
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+                    return dt;
+                }
+            }
+        }
+        public int Escribir(string query, Dictionary<string, object> parametros = null)
+        {
+            SqlConnection conexion = new SqlConnection(_connectionString);
+            conexion.Open();
+            SqlCommand cm = new SqlCommand();
+            using (cm = new SqlCommand(query, conexion))
+            {
+                if (parametros != null)
+                {
+                    foreach (var p in parametros)
+                    {
+                        cm.Parameters.AddWithValue(p.Key, p.Value ?? DBNull.Value);
+                    }
+                }
+            }
+            return cm.ExecuteNonQuery();
+        }
+    }
+}
