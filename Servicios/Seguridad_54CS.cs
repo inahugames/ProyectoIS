2725ddb3dfcaaae674e6ee156984452d1248a532
Nahu <nahucars100@outlook.com>
2026-04-27 15:19:30 -0300

    comienzo de implementacion de encriptacion utilizando BCrypt

---
+++ Servicios/Seguridad_54CS.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BCrypt;
+using BCrypt.Net;
+
+namespace Servicios
+{
+    public class Seguridad_54CS
+    {
+        public string EncriptarContraseña(string contra)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(contra);
+        }
+
+        public bool VerificarContraseña(string ingresada, string almacenada)
+        {
+            return BCrypt.Net.BCrypt.Verify(ingresada, almacenada);
+        }
+    }
+}
