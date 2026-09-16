2ded760552904c891d0811b6a0d70fab93698d1c
Nahu <nahucars100@outlook.com>
2026-09-16 14:59:32 -0300

    test

--- ProyectoIS/CambiarIdioma.cs
+++ ProyectoIS/CambiarIdioma.cs
@@ -15,6 +15,8 @@ namespace ProyectoIS
         {
             public string Codigo { get; set; }
             public string Nombre { get; set; }
+
+            public string test;
         }
 
         public CambiarIdioma()
